a09f34df0a9bb304742261c112c37ac5b0d7475e
Ishwor Khadka <Ishwor_Kiosked@Ishwors-MBP-2.lan>
2016-12-17 21:10:06 +0200

    improve composer.json

--- index.php
+++ index.php
@@ -4,3 +4,13 @@
  * @created 2016-12-14
  */
 
+require __DIR__ . '/vendor/autoload.php';
+
+$NavigableDate = \NavigableDate\NavigableDateFacade::create();
+
+var_dump($NavigableDate->format('Y-m-d H:i:s'));
+
+var_dump($NavigableDate->nextDay()->format('Y-m-d H:i:s'));
+var_dump($NavigableDate->format('Y-m-d H:i:s'));
+var_dump($NavigableDate->nextMonth()->format('Y-m-d H:i:s'));
+var_dump($NavigableDate->nextYear()->format('Y-m-d H:i:s'));
